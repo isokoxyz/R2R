45ed7f5c5883d6b89678e80ab1f4642fc37ca487
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2024-06-03 00:20:53 -0500

    download

--- ready2render/r2r/kadcars/download.py
+++ ready2render/r2r/kadcars/download.py
@@ -1,8 +1,10 @@
+import http.client
 import bpy
 import subprocess
 import requests
 import sys
 import os
+import http
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 
@@ -12,23 +14,39 @@ glb_ipfs_asset_file_name = ""
 glb_ipfs_asset_cid = ""
 asset_file_path = None
 
-# with requests.get("https://bafybeidyebmqeg6ibtpmww5hqjk7a5exddyqzxt4c6obaxm43hbz36kjke.ipfs.nftstorage.link/nft_0.glb", timeout=None, stream=True, verify=False) as r:
+client = http.client.HTTPSConnection("ipfs.io")
+# client.request("GET", "/ipfs/bafybeidyebmqeg6ibtpmww5hqjk7a5exddyqzxt4c6obaxm43hbz36kjke/nft_0.glb")
+client.request("GET", "/ipfs/bafybeidyebmqeg6ibtpmww5hqjk7a5exddyqzxt4c6obaxm43hbz36kjke")
+response = client.getresponse()
+# Check if the request was successful
+if response.status == 200:
+    # Read the data from the response
+    data = response.read()
+    print(data)
+
+    # Write the data to a file
+    # with open("lol.glb", 'wb') as file:
+    #     file.write(data)
+else:
+    print("fuck you")
+
+# with requests.get("https://ipfs.io/ipfs/bafybeidyebmqeg6ibtpmww5hqjk7a5exddyqzxt4c6obaxm43hbz36kjke/nft_0.glb", timeout=None, stream=True, verify=False) as r:
 #     r.raise_for_status()
-#     with open("./lol.glb", 'wb') as f:
+#     with open("./lol1.glb", 'wb') as f:
 #         for chunk in r.iter_content(chunk_size=None): 
 #             # If you have chunk encoded response uncomment if
 #             # and set chunk_size parameter to None.
 #             if chunk: 
 #                 f.write(chunk)
 
-session = requests.Session()
-with session.get("https://bafybeidyebmqeg6ibtpmww5hqjk7a5exddyqzxt4c6obaxm43hbz36kjke.ipfs.nftstorage.link/nft_0.glb") as r:
-    with open('lol.glb', 'wb') as f:
-        for chunk in r.iter_content(chunk_size=2048*2048): 
-            # If you have chunk encoded response uncomment if
-            # and set chunk_size parameter to None.
-            # if chunk: 
-            f.write(chunk)
+# session = requests.Session()
+# with session.get("https://bafybeidyebmqeg6ibtpmww5hqjk7a5exddyqzxt4c6obaxm43hbz36kjke.ipfs.nftstorage.link/nft_0.glb") as r:
+#     with open('lol.glb', 'wb') as f:
+#         for chunk in r.iter_content(chunk_size=2048*2048): 
+#             # If you have chunk encoded response uncomment if
+#             # and set chunk_size parameter to None.
+#             # if chunk: 
+#             f.write(chunk)
 # response = requests.get("https://ipfs.io/api/v0/get?arg="+glb_ipfs_dir_cid)
 
 # if response.status_code == 200:
